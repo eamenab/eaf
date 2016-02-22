6fdfd80e6cc936266d9cc622901fc66415f7a283
eamenab <yopo150@gmail.com>
2016-02-22 17:40:53 -0300

    Adding photo project

---
+++ client/js/eaf.js
@@ -0,0 +1,20 @@
+/*!
+ *
+ * JS done by EA
+ *
+ *
+ */
+
+// Load the gallery for the page
+$(document).ready(function() {
+$(".owl-carousel").owlCarousel({
+	navigation : true, // Show next and prev buttons
+	slideSpeed : 300,
+	paginationSpeed : 400,
+	items : 1, 
+    itemsDesktop : false,
+    itemsDesktopSmall : false,
+    itemsTablet: false,
+    itemsMobile : false
+});
+});
